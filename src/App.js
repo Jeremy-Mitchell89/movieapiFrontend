91c360797f5b88bcdb28b5e6777592b75e6cc801
Jeremy <Johnbovine@yahoo.com>
2019-02-13 20:20:10 -0600

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.scss";
+import axios from "axios";
 
 class App extends Component {
+  state = { data: {}, image: "" };
+  componentDidMount() {
+    axios
+      .get(
+        "https://api.themoviedb.org/3/movie/550?api_key=72ee3d3825c6b00b953e0fd471e755f2"
+      )
+      .then(res => {
+        console.log(res.data);
+        this.setState({ data: res.data });
+      });
+    axios
+      .get(`https://image.tmdb.org/t/p/w500${this.state.data.poster_path}`)
+      .then(res => {
+        console.log(res);
+        const image = res.data;
+        this.setState({ image });
+        return res.data;
+      });
+  }
   render() {
+    const { original_title, release_date, poster_path } = this.state.data;
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h2>{original_title}</h2>
+        <p>{release_date}</p>
+        <img
+          alt="Movie Poster"
+          src={`https://image.tmdb.org/t/p/w500${poster_path}`}
+        />
+
+        <p />
       </div>
     );
   }
