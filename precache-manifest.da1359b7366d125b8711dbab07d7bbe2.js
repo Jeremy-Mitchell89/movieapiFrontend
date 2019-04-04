7d2e3eb2e8174112e0cfc890205be7e742877ad5
Jeremy <Johnbovine@yahoo.com>
2019-04-03 19:48:56 -0500

    Updates

---
+++ precache-manifest.da1359b7366d125b8711dbab07d7bbe2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a06a3248f173f51bcbbcdd434a1de129",
+    "url": "/movieapiFrontend/static/media/white-waves.a06a3248.png"
+  },
+  {
+    "revision": "51f5a3e482d784d09eab",
+    "url": "/movieapiFrontend/static/js/runtime~main.51f5a3e4.js"
+  },
+  {
+    "revision": "deb78ed6c7d9fb41b1d7",
+    "url": "/movieapiFrontend/static/js/main.deb78ed6.chunk.js"
+  },
+  {
+    "revision": "0e071a4680c3106e39e5",
+    "url": "/movieapiFrontend/static/js/2.0e071a46.chunk.js"
+  },
+  {
+    "revision": "deb78ed6c7d9fb41b1d7",
+    "url": "/movieapiFrontend/static/css/main.1d9921c0.chunk.css"
+  },
+  {
+    "revision": "3c48b6d909d0e66f658cf31959de2de1",
+    "url": "/movieapiFrontend/index.html"
+  }
+];
